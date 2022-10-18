1f1f6063dd9a8b5a4c96f96cf6f8e8e53fd5e834
Prakash-Banjade <prakashbanjade191@gmail.com>
2022-10-18 12:28:10 +0545

    Updates

---
+++ precache-manifest.5991664d006f30eb60a6d38041ac5f28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "69919169fc290c630003",
+    "url": "/textplay/static/css/main.2673f72e.chunk.css"
+  },
+  {
+    "revision": "69919169fc290c630003",
+    "url": "/textplay/static/js/main.69919169.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/textplay/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "be092f9a53af46f9fdb9",
+    "url": "/textplay/static/js/2.be092f9a.chunk.js"
+  },
+  {
+    "revision": "19c6722eac4bf57ac98c",
+    "url": "/textplay/static/js/runtime~main.19c6722e.js"
+  },
+  {
+    "revision": "3e22ec261c5196debab4056636a5750c",
+    "url": "/textplay/index.html"
+  }
+];
